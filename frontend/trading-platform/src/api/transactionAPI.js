82be12d3d6b82f3d0f14947b609e8c6fbd171f39
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-04 10:46:16 +0800

    Update transactionAPI.js

--- frontend/trading-platform/src/api/transactionAPI.js
+++ frontend/trading-platform/src/api/transactionAPI.js
@@ -1,12 +1,10 @@
-import axios from "axios";
-
-const API_BASE_URL = "http://localhost:8000";
+import http from "./jwtHttp.js";
 
 async function fetchTransactions(user_id, skip = 0, limit = 25) {
     try {
-      const requestUrl = `${API_BASE_URL}/transactions/transactions?user_id=${user_id}&skip=${skip}&limit=${limit}`;
+      const requestUrl = `/transactions/transactions?user_id=${user_id}&skip=${skip}&limit=${limit}`;
   
-      const response = await axios.get(requestUrl);
+      const response = await http.get(requestUrl);
   
       console.log("Response Data:", response.data);
   
