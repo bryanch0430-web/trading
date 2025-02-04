8623c451b0d3fae3b67fbaf8bd6bee3dc102ba22
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-04 10:44:29 +0800

    Update jwtHttp.js

--- frontend/trading-platform/src/api/jwtHttp.js
+++ frontend/trading-platform/src/api/jwtHttp.js
@@ -1,7 +1,7 @@
 
 import jwtHttpInstanceFactory from "./jwtHttpInstanceFactory.js";
 
-const backend_url = import.meta.env.VITE_BACKEND_URL;
+const backend_url = "http://localhost:8000";
 const timeout = 30 * 1000;
 const baseURL = backend_url;
 
