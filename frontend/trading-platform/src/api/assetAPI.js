7926f9c6ad115d00dc70ae87eb11d65a172a8c4f
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-04 10:45:51 +0800

    Update assetAPI.js

--- frontend/trading-platform/src/api/assetAPI.js
+++ frontend/trading-platform/src/api/assetAPI.js
@@ -1,14 +1,11 @@
-import axios from "axios";
-
-const API_BASE_URL = "http://localhost:8000";
-
+import http from "./jwtHttp.js";
 
 
 async function ListAsset() {
     try {
-      const requestUrl = `${API_BASE_URL}/assets/list_all_asset`;
+      const requestUrl = `/assets/list_all_asset`;
   
-      const response = await axios.get(requestUrl);
+      const response = await http.get(requestUrl);
   
   
       return response.data
@@ -20,9 +17,9 @@ async function ListAsset() {
 
   async function ListAssetPrice(assetId){
     try {
-      const requestUrl = `${API_BASE_URL}/assets/get_asset_price/${assetId}`;
+      const requestUrl = `/assets/get_asset_price/${assetId}`;
   
-      const response = await axios.get(requestUrl);
+      const response = await http.get(requestUrl);
   
       return response.data
     } catch (error) {
@@ -34,8 +31,8 @@ async function ListAsset() {
 
   async function getUserAssets(userId) {
     try {
-      const requestUrl = `${API_BASE_URL}/assets/get_user_asset?user_id=${userId}`;
-      const response = await axios.get(requestUrl);
+      const requestUrl = `/assets/get_user_asset?user_id=${userId}`;
+      const response = await http.get(requestUrl);
       return response.data;
     } catch (error) {
       console.error(`Error fetching assets for user ID ${userId}:`, error);
