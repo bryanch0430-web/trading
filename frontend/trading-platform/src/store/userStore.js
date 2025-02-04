e959e339be4ae590a350a004119f46d1f950511f
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-04 11:06:42 +0800

    Update userStore.js

--- frontend/trading-platform/src/store/userStore.js
+++ frontend/trading-platform/src/store/userStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
-import { ListUserAsset } from '../api/userAPI';
+import userAPI from '../api/userAPI';
 
-export const useUserStore = defineStore('transaction', {
+export const useUserStore = defineStore('user', {
   state: () => ({
     userAsset: [],
     loading: false, // Add a loading state
@@ -15,7 +15,7 @@ export const useUserStore = defineStore('transaction', {
     async fetchUserAssets(user_id = '2fb3c95f-0250-4c9c-8194-0e22bdf1ae32') {
       this.loading = true; // Set loading to true when fetching starts
       try {
-        const response = await ListUserAsset(user_id);
+        const response = await userAPI.ListUserAsset(user_id);
         this.userAsset = response;
       } catch (error) {
         console.error('Error fetching transactions:', error);
