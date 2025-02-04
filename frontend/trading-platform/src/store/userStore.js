20f9ff454f8717f4fb4020f189c295aab10e8b2d
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-04 14:43:39 +0800

    Update userStore.js

--- frontend/trading-platform/src/store/userStore.js
+++ frontend/trading-platform/src/store/userStore.js
@@ -4,11 +4,15 @@ import userAPI from '../api/userAPI';
 export const useUserStore = defineStore('user', {
   state: () => ({
     userAsset: [],
+    valueTrend:[],
     loading: false, // Add a loading state
   }),
   getters: {
     allAsset(state) {
       return state.userAsset;
+    },
+    getValueTrend(state) {
+      return state.valueTrend;
     },
   },
   actions: {
@@ -17,6 +21,18 @@ export const useUserStore = defineStore('user', {
       try {
         const response = await userAPI.ListUserAsset(user_id);
         this.userAsset = response;
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+      } finally {
+        this.loading = false; // Set loading to false when fetching ends
+      }
+    },
+
+    async ShowUserValueTrend(user_id = '2fb3c95f-0250-4c9c-8194-0e22bdf1ae32') {
+      this.loading = true; // Set loading to true when fetching starts
+      try {
+        const response = await userAPI.ShowUserValueTrend(user_id);
+        this.valueTrend = response;
       } catch (error) {
         console.error('Error fetching transactions:', error);
       } finally {
