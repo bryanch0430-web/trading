f06d0a7c1ed866ee9c3e5a6d1d3d89a02df4acc4
bryanch0430-web <129389913+bryanch0430-web@users.noreply.github.com>
2025-02-05 13:33:45 +0800

    Create authStore.js

---
+++ frontend/trading-platform/src/store/authStore.js
@@ -0,0 +1,62 @@
+import { defineStore } from 'pinia';
+import authApi from "../api/authAPI.js";
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: localStorage.getItem('access_token') || '',
+    user: null,
+    userName: '',
+  }),
+  actions: {
+    async login(username, password) {
+      try {
+        const payload={
+            username:username,
+            password:password
+
+        }
+        const response = await authApi.getLoginToken(payload);
+
+        this.token = response.access_token;
+        localStorage.setItem('access_token', this.token);
+
+        // // Fetch user profile after login
+        await this.fetchUser();
+
+        return true;
+      } catch (error) {
+        console.error('Login failed:', error);
+        throw error;
+      }
+    },
+    logout() {
+      this.token = '';
+      this.user = null;
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('userId');
+      localStorage.removeItem('userName');
+
+    },
+    async fetchUser() {
+      try {
+        // const token = localStorage.getItem('access_token');
+
+        const userResponse = await authApi.getUserProfile();
+        console.log('User profile:', userResponse);
+        this.user = userResponse;
+        localStorage.setItem('userName', this.user.username);
+        localStorage.setItem('userId', this.user.id);
+
+      } catch (error) {
+        console.error('Failed to fetch user:', error);
+        this.logout();
+      }
+    },
+
+  },
+  getters: {
+    isLoggedIn: (state) => !!state.token,
+  }
+});
+
+export default useAuthStore;
